e2f62244b058ad59628e525c31287cd1bed2d081
abrilbabino <abril.nadia.babino984@gmail.com>
2026-03-20 18:57:29 -0300

    arreglado

--- api/main.py
+++ api/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 from TrabajoPractico1.Hit6.NodoD import router1
-from TrabajoPractico1.Hit7.NodoD import router2
 
 
 app = FastAPI()
 
 app.include_router(router1)
-app.include_router(router2)
+
 
 
 @app.get("/")
