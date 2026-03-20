6e013ffe0ee0edce9a38c9f6fb29521225596042
Naiara Collazo <collazonaiara@gmail.com>
2026-03-20 13:35:50 -0300

    Agrego logger

--- TrabajoPractico1/Hit3/NodoB.py
+++ TrabajoPractico1/Hit3/NodoB.py
@@ -1,6 +1,7 @@
 import socket
 import os
 from dotenv import load_dotenv
+from ..common.logger import log_event
 
 # cargo las variables del .env
 load_dotenv()
@@ -9,6 +10,8 @@ HOST, PORT = os.getenv("SERVER_1_ADDR_TP1").split(":")
 PORT = int(PORT)
 
 def start_server(stop_event):
+    log_event("INFO", "Nodo B (servidor resiliente) iniciado")
+
     # Creo el socket con el tipo de direccionamiento ipv4 (AF_INET) y el tipo de protocolo (SOCK_TREAM para TCP)
     NodoB = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -21,33 +24,33 @@ def start_server(stop_event):
 
     NodoB.settimeout(1)
 
-    print(f"Server (NodoB) Listening on {HOST}:{PORT} ...")
+    log_event("INFO", f"Servidor escuchando en {HOST}:{PORT}")
 
   # Loop infinito para aceptar múltiples conexiones. el stop event es para finalizar explicitamente el servidor luego de los test y que no quede levantado.
     while not stop_event.is_set():
         try:
             conn, addr = NodoB.accept()
+            log_event("INFO", f"Conexion recibida desde {addr}")
+
         except socket.timeout:
             continue #vuelve a checkear el stop event
         
         with conn:
-            print(f"Conexion recibida desde {addr}")
+            log_event("INFO", f"Conexion recibida desde {addr}")
 
             while True:
                 data = conn.recv(1024).decode()
                 if data == "":
                     # si el cliente cerró la conexión, salgo del loop interno
-                    print(f"Cliente {addr} desconectado.")
+                    log_event("INFO", f"Cliente {addr} desconectado")
                     conn.close()
                     break
-
-                print(f"Cliente Dice: {data}")
+                log_event("INFO", f"Mensaje recibido de {addr}: {data}")
                 response = "Mensaje Recibido"
                 conn.sendall(response.encode())
-
-    
+                log_event("INFO", f"Respuesta enviada a {addr}")
     NodoB.close()
-    print ("Servidor cerrado correctamente")
+    log_event("INFO", "Servidor cerrado correctamente")
 
 if __name__ == "__main__":
     start_server()
--- TrabajoPractico1/Hit5/NodoC.py
+++ TrabajoPractico1/Hit5/NodoC.py
@@ -4,6 +4,7 @@ import os
 import time
 import json
 from dotenv import load_dotenv
+from ..common.logger import log_event
 
 # cargo las variables del .env
 load_dotenv()
@@ -13,7 +14,7 @@ RETRY_DELAY = int(os.getenv("RETRY_DELAY"))
 
 def handle_conn(conn, addr):
 
-    print(f"[SERVER] Conectado con {addr}")
+    log_event("INFO", f"[SERVER] Conexion establecida con {addr}")
 
     try:
        
@@ -22,12 +23,12 @@ def handle_conn(conn, addr):
             data = conn.recv(1024)
 
             if not data:
+                log_event("INFO", f"[SERVER] Cliente {addr} desconectado")
                 break
 
             # deserializo el mensaje con formato json para obtener un diccionario.
             msg = json.loads(data.decode())
-
-            print("[SERVER] Mensaje Recibido: " + msg["msg"])
+            log_event("INFO", f"[SERVER] Mensaje recibido de {addr}: {msg}")
 
             response = {
                 "type"  : "msgRecibido",
@@ -36,16 +37,18 @@ def handle_conn(conn, addr):
 
             # serializo la respuesta
             response_json = json.dumps(response)
-
             conn.sendall(response_json.encode())
+            log_event("INFO", f"[SERVER] Respuesta enviada a {addr}: {response}")
+
 
     finally:
         conn.close()
-        print(f"[SERVER] Conexion cerrada con {addr}")
+        log_event("INFO", f"[SERVER] Conexion cerrada con {addr}")
 
 
 def start_server(host, port, stop_event):
     # Creo el socket con el tipo de direccionamiento ipv4 (AF_INET) y el tipo de protocolo (SOCK_TREAM para TCP)
+    log_event("INFO", f"Servidor iniciando en {host}:{port}")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Esto permite reutilizar el puerto para que el SO no lo ponga en time wait al ejecutar varias pruebas seguidas
@@ -57,13 +60,14 @@ def start_server(host, port, stop_event):
 
     server.settimeout(1)
 
-    print(f"Server Listening on {host}:{port} ...")
+    log_event("INFO", f"Server listening on {host}:{port}")
 
   # Loop infinito para aceptar múltiples conexiones
     while not stop_event.is_set():
 
         try:
             conn, addr = server.accept()
+            log_event("INFO", f"Nueva conexion desde {addr}")
         except socket.timeout:
             continue
         
@@ -77,10 +81,12 @@ def start_server(host, port, stop_event):
         server_thread.start()
 
     server.close()
-    print(f"Server {host}:{port} cerrado")
+    log_event("INFO", f"Servidor {host}:{port} cerrado")
 
 
 def start_client(target_host, target_port):
+
+    log_event("INFO", f"[CLIENT] Iniciando cliente hacia {target_host}:{target_port}")
 
     # meto la conexion del cliente en un loop infinito
     while True:
@@ -88,16 +94,18 @@ def start_client(target_host, target_port):
         # Intenta conectarse al servidor y si no puede se queda reintentando
         try:
             client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            log_event("INFO", "[CLIENT] Intentando conectar...")
             client.connect((target_host, target_port))
-            print("[CLIENT] Conectado al servidor")
+            log_event("INFO", "[CLIENT] Conectado al servidor")
             msg = {
                 "type": "ClienteConectado",
                 "msg": "[CLIENTE] Me conecte",
             }
             # serializo msg para convertirlo en json
             msg_json = json.dumps(msg)
-
             client.sendall(msg_json.encode())
+            log_event("INFO", f"[CLIENT] JSON Enviado: {msg}")
+
 
             while True:
 
@@ -108,13 +116,14 @@ def start_client(target_host, target_port):
                     raise ConnectionError("conexion cerrada")
 
                 response = json.loads(response_json)
+                log_event("INFO", f"[CLIENT] Respuesta recibida: {response}")
+
                 client.close()
+                log_event("INFO", "[CLIENT] Conexion cerrada")
+
 
                 return response["msg"]
         except ConnectionRefusedError:
 
-            print("[CLIENT] Servidor no disponible, reintentando en {RETRY_DELAY} segundos...")
+            log_event("ERROR", f"[CLIENT] Servidor no disponible, reintentando en {RETRY_DELAY} segundos...")
             time.sleep(RETRY_DELAY)
-
-
-        
